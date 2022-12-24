92cfe0ccb7599b98a6dd39ce0034a7b14c7cba05
Indrajit <indrajitghosh912@gmail.com>
2022-12-24 06:20:55 +0530

    initial commit

---
+++ encryption.py
@@ -0,0 +1,14 @@
+# Codes related to encryption
+#
+# Author: Indrajit Ghosh
+#
+# Created on: Dec 24, 2022
+#
+
+
+def main():
+    print('hello world!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,16 @@
+# Script to encrypt/decrypt a file (or dir)
+#
+# Author: Indrajit Ghosh
+#
+# Created on: Dec 24, 2022
+#
+
+from pathlib import Path
+import subprocess, sys, os, time
+
+def main():
+    print('hello world!')
+
+
+if __name__ == '__main__':
+    main()
